7c375978c276f2a547e1bc00172ded2e083304e5
Leo Lvcdb <leo.levancanhditban.etu@univ-lille.fr>
2021-11-02 23:19:38 +0100

    Mise a jour

--- save/lib/DataLayer.class.php
+++ save/lib/DataLayer.class.php
@@ -3,21 +3,26 @@
 //Grzechowiak Adrien
 //groups 1
 class DataLayer {
-	// private ?PDO $conn = NULL; // le typage des attributs est valide uniquement pour PHP>=7.4
+	//private ?PDO $conn = NULL; // le typage des attributs est valide uniquement pour PHP>=7.4
 
-	private  $connexion = NULL; // connexion de type PDO   compat PHP<=7.3
+	private  $conn = NULL; // connexion de type PDO   compat PHP<=7.3
 	
 	/**
 	 * @param $DSNFileName : file containing DSN 
 	 */
 	function __construct(string $DSNFileName){
-		$dsn = "uri:$DSNFileName";
-		$this->connexion = new PDO($dsn);
+		//$dsn = "uri:$DSNFileName";
+		$dsn = 'mysql:host=51.210.137.135;';
+		$user = 'root';
+		$password = 'uaj7hh1t';
+
+		$dbh = new PDO($dsn, $user, $password);
+
 		// paramètres de fonctionnement de PDO :
-		$this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // déclenchement d'exception en cas d'erreur
-		$this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC); // fetch renvoie une table associative
+		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // déclenchement d'exception en cas d'erreur
+		$this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC); // fetch renvoie une table associative
 		// réglage d'un schéma par défaut :
-		$this->connexion->query('set search_path=resto_u, authent');
+		$this->conn->query('set search_path=eatngo, authent');
 	}
     
 	/**
@@ -31,7 +36,7 @@ class DataLayer {
 		* max_lat, max_lon
 	 */
 	function getRest(): array {
-		$sql = "select nom,adresse,position,status,afluence from resto ";
+		$sql = "select * from resto ";
 		$stmt = $this->connexion->prepare($sql);
 		$stmt->execute();
 		$res= $stmt->fetchAll();
